d74e10e8e5d6941a186677481b1f970546181ab2
Osunronbi Okikiola <osunronbiolanlesi@gmail.com>
2022-11-27 18:33:11 +0100

    changed title in viewcontroller and rebuilt js

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -7,7 +7,7 @@ const Booking = require('../models/bookingModel');
 exports.getOverview = catchAsync(async (req, res, next) => {
   const tours = await Tour.find();
   res.status(200).render('overview', {
-    title: 'The Forest Hiker',
+    title: 'All Tours',
     tours,
   });
 });
